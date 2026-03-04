186f38880645661aa3afc361b27f959da32d35f6
Lucas Liaño <lliano@frba.utn.edu.ar>
2026-03-04 19:29:30 -0300

    fix: Removing tasks before OTA.

--- main/src/gpio.c
+++ main/src/gpio.c
@@ -2,6 +2,11 @@
 
 static const char *TAG = "RELAY_TASK";
 
+extern TaskHandle_t led_task_Handle;
+extern TaskHandle_t bme280_task_Handle;
+extern TaskHandle_t victron_ble_task_Handle;
+extern TaskHandle_t custom_metrics_task_Handle;
+
 esp_err_t gpio_init(void)
 {
     gpio_config_t io_conf = {
@@ -35,7 +40,18 @@ void relay_task(void *pvParameter)
         vTaskDelay(pdMS_TO_TICKS(RELAY_OFF_MS));
 
         gpio_set_level(RELAY_GPIO_NUM, 1);
-        vTaskDelay(pdMS_TO_TICKS(120*1000));    // Delay to let starlink initialize
+        // vTaskDelay(pdMS_TO_TICKS(120*1000));    // Delay to let starlink initialize
+        vTaskDelay(pdMS_TO_TICKS(3*1000));    // Delay to let starlink initialize
+
+
+        // Stops all tasks to avoid conflicts while running OTA
+        vTaskDelete(led_task_Handle);
+        vTaskDelete(bme280_task_Handle);
+        vTaskDelete(victron_ble_task_Handle);
+        vTaskDelete(custom_metrics_task_Handle);
+        vTaskDelay(pdMS_TO_TICKS(10));    // Delay to let starlink initialize
+
+
         ESP_LOGI(TAG, "Running OTA..");
         if (wifi_connect_if_needed() == ESP_FAIL) 
         {
